bbba46bdb32876158e263a9281448da40ebf6126
WILLWANG <wangziweichn@outlook.com>
2019-05-22 18:37:48 +0800

    增加icon

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,28 @@
 <template>
-    <button class="g-button">button</button>
+    <div>
+        <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+            <svg v-if="icon" class="icon">
+                <use v-bind:xlink:href="`#icon-${icon}`"></use>
+            </svg>
+            <div class="content">
+                <slot></slot>
+            </div>
+        </button>
+    </div>
 </template>
 <script>
-    export default {}
+    export default {
+        props: {
+            icon:{},
+            iconPosition:{
+                type:String,
+                default:'left',
+                validator(value){
+                    return value === 'left' || value === 'right';
+                }
+            }
+        }
+    }
 </script>
 <style lang="scss">
     .g-button {
@@ -12,7 +32,9 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
-
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
         &:hover {
             border-color: var(--border-color-hover);
         }
@@ -23,6 +45,12 @@
 
         &:focus {
             outline: none;
+        }
+        > .content{order:2;}
+        > .icon{order:1;margin-right: 0.3em;margin-left: 0em;}
+        &.icon-right{
+            > .content{order:1;}
+            > .icon{order:2;margin-right: 0em;margin-left: 0.3em;}
         }
     }
 </style>
