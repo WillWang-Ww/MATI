d0a8f859e8155b08c1d350a6f7dd7668cd5454a5
WILLWANG <wangziweichn@outlook.com>
2019-06-03 20:42:09 +0800

    添加toast和plugin

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import ButtonGroup from './button-group'
 import Input from './input'
 import Row from './row'
 import Col from './col'
+import Toast from './toast'
+import plugin from './plugin'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon',Icon)
@@ -12,6 +14,8 @@ Vue.component('g-button-group',ButtonGroup)
 Vue.component('g-input',Input)
 Vue.component('g-row',Row)
 Vue.component('g-col',Col)
+Vue.component('g-toast',Toast)
+Vue.use(plugin)
 
 new Vue({
     el: '#app',
@@ -24,7 +28,8 @@ new Vue({
     create(){
     },
     methods:{
-        inputChange(){
+        showToast(){
+            this.$toast('I am message')
         }
     }
 })
---
+++ src/toast.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="toast">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name:'MatiToast'
+    }
+</script>
+<style scoped lang="scss">
+    $font-size:14px;
+    $toast-height:40px;
+    $toast-bg:rgba(0,0,0,0.75);
+    .toast {
+        font-size: $font-size;
+        height: $toast-height;
+        line-height: 1.8;
+        display: flex;
+        align-content: center;
+        position: fixed;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        color: white;
+        background:$toast-bg;
+        border-radius: 4px;
+        box-shadow: 0 0 3px 0 rgba(0,0,0,0.50);
+        padding: 0 16px;
+    }
+</style>
