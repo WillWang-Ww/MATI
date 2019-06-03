b53da410d9f97cb4bd1db0047d6c388f7ab83167
WILLWANG <wangziweichn@outlook.com>
2019-06-03 23:45:59 +0800

    解决currentToast的一个BUG

--- src/plugin.js
+++ src/plugin.js
@@ -8,16 +8,24 @@ export default {
             if (currentCoast) {
                 currentCoast.close()
             }
-            currentCoast = createCoast({Vue, message, propsData: toastOptions})
+            currentCoast = createCoast({
+                Vue,
+                message,
+                propsData: toastOptions,
+                onClose:()=>{
+                    currentCoast = null
+                }
+            })
         }
     }
 }
 
-function createCoast({Vue, message, propsData}) {
+function createCoast({Vue, message, propsData,onClose}) {
     let Constructor = Vue.extend(Toast)
     let toast = new Constructor({propsData})
     toast.$slots.default = [message]
     toast.$mount()
+    toast.$on('close',onClose)
     document.body.appendChild(toast.$el)
     return toast
 }
