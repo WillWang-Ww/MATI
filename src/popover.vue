e258501650c540e1749ebcad803750ab703c0f59
WILLWANG <wangziweichn@outlook.com>
2019-06-08 16:40:48 +0800

    将popover放到body下面，解决overflow hidden问题

--- src/popover.vue
+++ src/popover.vue
@@ -1,30 +1,37 @@
 <template>
     <div class="popover" @click.stop="xxx">
-        <div class="content-wrapper" v-if="visible" @click.stop>
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 
 <script>
     export default {
         name: "MatiPopover",
-        data(){
-            return{
-                visible:false
+        data() {
+            return {
+                visible: false
             }
         },
-        methods:{
-            xxx(){
+        methods: {
+            xxx() {
                 this.visible = !this.visible
-                if (this.visible === true){
-                    this.$nextTick(()=>{
+                if (this.visible === true) {
+                    this.$nextTick(() => {
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+                        console.log(this.$refs.contentWrapper.style.left);
                         let eventHandler = () => {
                             this.visible = false
-                            document.removeEventListener('click',eventHandler)
+                            document.removeEventListener('click', eventHandler)
                         }
-                        document.addEventListener('click',eventHandler)
+                        document.addEventListener('click', eventHandler)
                     })
                 }
             }
@@ -32,16 +39,15 @@
     }
 </script>
 <style scoped lang="scss">
-    .popover{
+    .popover {
         display: inline-block;
         vertical-align: top;
         position: relative;
-        .content-wrapper{
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            border: 1px solid black;
-            box-shadow: 0 0 3px rgba(0,0,0,0.5);
-        }
+    }
+    .content-wrapper {
+        position: absolute;
+        border: 1px solid black;
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        transform: translateY(-100%);
     }
 </style>
