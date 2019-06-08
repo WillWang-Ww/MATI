4d2b080426dd69d9dbb209feaaa554d758720fd2
WILLWANG <wangziweichn@outlook.com>
2019-06-08 18:10:22 +0800

    完成基本功能

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="popover" @click.stop="xxx">
+    <div class="popover" @click.stop="onClick" ref="popover">
         <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
@@ -18,21 +18,36 @@
             }
         },
         methods: {
-            xxx() {
-                this.visible = !this.visible
-                if (this.visible === true) {
-                    this.$nextTick(() => {
-                        document.body.appendChild(this.$refs.contentWrapper)
-                        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-                        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-                        console.log(this.$refs.contentWrapper.style.left);
-                        let eventHandler = () => {
-                            this.visible = false
-                            document.removeEventListener('click', eventHandler)
-                        }
-                        document.addEventListener('click', eventHandler)
-                    })
+            positionContent() {
+                document.body.appendChild(this.$refs.contentWrapper)
+                let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+            },
+            onClickDocument (e){
+                if (this.$refs.contentWrapper.contains(e.target)) {
+                    return
+                }
+                this.close()
+            },
+            open() {
+                this.visible = true
+                this.$nextTick(() => {
+                    this.positionContent()
+                    document.addEventListener('click', this.onClickDocument)
+                })
+            },
+            close(){
+                this.visible = false
+                document.removeEventListener('click', this.onClickDocument)
+            },
+            onClick(event) {
+                if (this.$refs.triggerWrapper.contains(event.target)) {
+                    if (this.visible === true) {
+                        this.close()
+                    }else{
+                        this.open()
+                    }
                 }
             }
         }
@@ -44,6 +59,7 @@
         vertical-align: top;
         position: relative;
     }
+
     .content-wrapper {
         position: absolute;
         border: 1px solid black;
